595cf81174044f5537e35aba96e6e637fae3286a
Jon Williams <jonathan.williams@gmail.com>
2009-06-18 15:07:54 -0400

    test directory added

---
+++ test/unit/sfDomFeedTest.php
@@ -0,0 +1,6 @@
+<?php
+
+include(dirname(__FILE__).'/../../../../test/bootstrap/unit.php');
+require_once(dirname(__FILE__).'/../../lib/sfDomFeed.class.php');
+
+$t = new lime_test(0, new lime_output_color());
