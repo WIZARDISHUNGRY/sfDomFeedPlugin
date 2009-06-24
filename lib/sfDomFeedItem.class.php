b5767a74adfa935c15c28d398848082b5307b3c8
Jon Williams <jonathan.williams@gmail.com>
2009-06-24 16:22:47 -0400

    need this to be fully func

---
+++ lib/sfDomFeedItem.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the sfDomFeedPlugin package.
+ * (c) 2009 Jon Williams <jwilliams@limewire.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfDomFeedItem.
+ *
+ * @package    sfDomFeed
+ * @author     Jon Williams <jwilliams@limewire.com>
+ */
+class sfDomFeedItem extends sfDomStorage
+{
+}
