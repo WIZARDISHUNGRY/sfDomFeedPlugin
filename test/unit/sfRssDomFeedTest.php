29f58f04d5a3f3c45ffcce319c760b6c2e2737ce
Jon Williams <jonathan.williams@gmail.com>
2009-08-04 17:23:57 -0400

    I've had this stub of a unit test in my tree for too long

---
+++ test/unit/sfRssDomFeedTest.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once(dirname(__FILE__).'/../bootstrap/unit.php');
+
+require_once(dirname(__FILE__).'/../../lib/sfDomFeed.class.php');
+require_once(dirname(__FILE__).'/../../lib/sfRssDomFeed.class.php');
+
+$t = new lime_test(0, new lime_output_color());
+
+$feed_params = array(
+  'title' => 'foo', 
+  'link' => 'bar', 
+  'description' => 'foobar baz',
+  'language' => 'fr', 
+  'authorName' => 'francois',
+  'authorEmail' => 'francois@toto.com',
+  'authorLink' => 'http://francois.toto.com',
+  'subtitle' => 'this is foo bar',
+  'categories' => array('foo', 'bar'),
+  'feedUrl' => 'http://www.example.com',
+  'encoding' => 'UTF-16',
+);
+
+$feed=new sfRssDomFeed();
+// RSS specific code here
