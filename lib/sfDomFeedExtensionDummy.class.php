7305335ceacb92dad9598ab8877da1d4a5d73fc2
Jon Williams <jonathan.williams@gmail.com>
2009-08-05 17:51:55 -0400

    dummy rss extension

---
+++ lib/sfDomFeedExtensionDummy.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the sfDomFeedPlugin package.
+ * (c) 2009 Jon Williams <jwilliams@limewire.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfDomFeedExtensionDummy.
+ *
+ * @package    sfDomFeed
+ * @author     Jon Williams <jwilliams@limewire.com>
+ */
+class sfDomFeedExtensionDummy extends sfDomFeedExtension
+{
+
+}
