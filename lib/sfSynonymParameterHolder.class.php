ed3f9f3b898d30c3e60e26d60a220df726e29597
Jon Williams <jonathan.williams@gmail.com>
2009-08-18 15:26:42 -0400

    a stub of synonmyming

---
+++ lib/sfSynonymParameterHolder.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the sfDomFeedPlugin package.
+ * (c) 2009 Jon Williams <jwilliams@limewire.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfProxyParameterHolder adds support for synonyms of common words
+ * @package    sfSynonymParameterHolder
+ * @author     Jon Williams <jwilliams@limewire.com>
+ */
+class sfSynonymParameterHolder extends sfProxyParameterHolder
+{
+  protected static $dictionary=null; // second sfParameterHolder instance!
+  public function __construct($object=null)
+  {
+    if(is_null(self::$dictionary))
+    {
+      self::$dictionary=new sfParameterHolder();
+      self::$dictionary->add($this->loadDictionary());
+    }
+    $this->wrapped_object=$object;
+  }
+
+  public function loadDictionary()
+  {
+   $plugin_path=realpath(dirname(__FILE__).'/../'); 
+    return Array(
+      'length'=>Array(
+        'size',
+      ),
+    );
+  }
+}
